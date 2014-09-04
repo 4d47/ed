f87317fee557fe1bdb1539cc95e063172d23cd2f
Mathieu Gagnon <mathieu@gagnon.name>
2014-09-04 12:11:45 -0400

    Fix TableResourceTest param

--- tests/Ed/TableResourceTest.php
+++ tests/Ed/TableResourceTest.php
@@ -7,6 +7,6 @@ class TableResourceTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertEquals(array(), TableResource::match('/'));
         $this->assertEquals(array('table' => 'foo'), TableResource::match('/foo'));
-        $this->assertEquals(array('table' => 'foo', 'key' => '', 'extension' => 'json'), TableResource::match('/foo.json'));
+        $this->assertEquals(array('table' => 'foo', 'id' => '', 'extension' => 'json'), TableResource::match('/foo.json'));
     }
 }
